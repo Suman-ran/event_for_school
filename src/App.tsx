82b4728fc6775103f9e737009af1282ce35c940d
Suman Rana <mentor014@yuvipep.com>
2025-07-26 17:53:27 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App = () => {
           <Sonner />
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Index />} />
+              <Route path="/event_for_school" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
