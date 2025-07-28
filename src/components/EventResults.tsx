7f2db2da0067de1d3c1774861c7810e1a634be03
Suman Rana <mentor014@yuvipep.com>
2025-07-28 14:37:07 +0530

    Fix: Add color in event home result

--- src/components/EventResults.tsx
+++ src/components/EventResults.tsx
@@ -19,7 +19,7 @@ const EventResults = () => {
       'Delany': 'bg-green-600 text-red-800 border-red-200',
       'Gandhi': 'bg-yellow-500 text-blue-800 border-blue-200',
       'Tagore': 'bg-blue-600 text-green-800 border-green-200',
-      'Aloysius': 'bg-purple-700 text-yellow-800 border-yellow-200'
+      'Nehru': 'bg-purple-700 text-blue-800 border-yellow-200'
     };
     return colors[house as keyof typeof colors] || 'bg-gray-100 text-gray-800';
   };
